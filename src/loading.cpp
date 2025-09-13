9f90680c0846739acc98773eeb1354db6842cbfc
Lizicai <shaoxin5345@gmail.com>
2025-09-13 17:06:10 +0800

    add loading页内容

---
+++ src/loading.cpp
@@ -0,0 +1,74 @@
+#include "loading.h"
+
+Loading loading;
+
+Loading::Loading(){
+  barX = PANEL_WIDTH * PANEL_CHAIN * 0.2 / 2;
+  barY = PANEL_HEIGHT * PANEL_CHAIN * 0.78;
+  midPaneEhight = PANEL_HEIGHT * PANEL_CHAIN / 2;
+  titleX = (PANEL_WIDTH * PANEL_CHAIN - (7 * 11 + 10)) / 2;
+  errMsgX = (PANEL_WIDTH * PANEL_CHAIN - (7 * 11 + 10)) / 2 - 10;
+  barWidth = PANEL_WIDTH * 0.8;
+  barHeight = 8;
+  radius = 1;
+  flag = true; // ✅ 默认就是 true
+  message = "";
+  duration = DURATION;
+}
+
+void Loading::drawFrame() {
+    dma_display->fillRoundRect(barX, barY, barWidth, barHeight,radius, dma_display->color565(150, 150, 150));
+    dma_display->flipDMABuffer();
+}
+
+void Loading::updateProgress(float percent) {
+    int fillWidth =
+        (int)((barWidth - 2) * percent + 0.5f);
+    dma_display->fillRect(barX + 1, barY + 1, barWidth - 2, barHeight - 2,
+                          dma_display->color565(0, 0, 0));
+   if (fillWidth > 0) {
+      dma_display->fillRect(barX + 1, barY + 1, fillWidth, barHeight - 2,
+                            dma_display->color565(0, 200, 0));
+    }
+}
+
+void Loading::showSetupMsg(){
+    display.displayTextRGB(0, 255, 0, 0, 15, u8g2_font_wqy13_t_gb2312, SETUP_WIFI_MSG);
+    display.displayTextRGB(0, 255, 0, 0, 30, u8g2_font_6x10_tf, SETUP_WIFI_NAME);
+    display.displayTextRGB(0, 255, 0, 0, 45, u8g2_font_wqy13_t_gb2312, SETUP_WIFI_ADDRESS);
+}
+
+void Loading::drawTitle() {
+    display.displayTextRGB(0, 255, 0, titleX, midPaneEhight, u8g2_font_7x13B_tf, "MatrixClock");
+}
+
+void Loading::drawErrMsg() {
+    display.displayTextRGB(255, 0, 0, errMsgX, midPaneEhight + 13, u8g2_font_6x10_tf, message.c_str());
+}
+
+void Loading::flipDMABuffer(){
+    dma_display->flipDMABuffer();
+}
+void Loading::setFlag(bool value) {
+    flag = value;
+}
+
+bool Loading::getFlag() const {
+    return flag;
+}
+
+void Loading::setMessage(String msg) {
+    message = msg;
+}
+
+String Loading::getMessage(){
+    return message;
+}
+
+void Loading::setDuration(unsigned long dur){
+    duration = dur;
+}
+
+unsigned long Loading::getDuration(){
+    return duration;
+}
---
+++ src/loading.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+#include <U8g2_for_Adafruit_GFX.h>
+#include "common_define.h"
+#include "display.h"
+
+// 外部全局对象
+extern MatrixPanel_I2S_DMA *dma_display;
+extern U8G2_FOR_ADAFRUIT_GFX u8g2_for_adafruit_gfx;
+
+// Loading 类
+class Loading {
+public:
+    Loading();
+
+    void drawFrame();   // 方法一: 绘制进度条外框
+    void drawTitle();        // 方法二: 绘制标题
+    void drawErrMsg();
+    void updateProgress(float percent); // 方法三: 更新进度条
+    void showSetupMsg();
+    void flipDMABuffer();
+
+    void setFlag(bool flag);  // 设置标记
+    bool getFlag() const;     // 获取标记
+
+    void setMessage(String message);
+    String getMessage();
+
+    void setDuration(unsigned long duration);
+    unsigned long getDuration();
+
+  private:
+    int barX, barY, midPaneEhight, titleX, errMsgX, barWidth, barHeight, radius;
+    unsigned long duration;
+    String message;
+    bool flag; // true = 正常加载，false = 暂停
+};
+
+// ✅ 全局对象声明
+extern Loading loading;
